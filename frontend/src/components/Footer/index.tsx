c252e3d4019812e295c3aec8cf9e7fd3bf67d337
Dennis Ribeiro <dennis_fdr@hotmail.com>
2021-09-14 03:35:36 -0300

    teste

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/dennis-ribeiro-24956868" target="_blank" rel="noreferrer">EAGLES AUTOMATION</a></p>
+                <p className="text-light"><small><strong>Predponta</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/predpontamca" target="_blank" rel="noreferrer">@predpontamca</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
